eec4273ed75e0b7e1179779ec5290d035420ccf9
QuiiBz <tom.lienrd@gmail.com>
2021-12-15 13:55:08 +0100

    feat: fill home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,19 @@
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home: FC = () => {
-  return <h1>Hello</h1>;
+  const navigate = useNavigate();
+
+  const play = () => navigate('/play');
+
+  return (
+    <div className="flex flex-col items-center m-x-auto">
+      <h1 className="text-2xl mb-8 text-center">Welcome to Hangman!</h1>
+      <button type="button" onClick={play} className="px-4 py-2 bg-sky-400 hover:bg-sky-500 text-white rounded">
+        Play now
+      </button>
+    </div>
+  );
 };
 
 export default Home;
